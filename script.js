3b154a8ead4e291beb85844307b004fa492028ef
Samuel Silva <samuelsljr@gmail.com>
2025-11-16 14:55:55 -0300

    pusher 50 ºth

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ function calculate() {
         $("#invalidmsg").stop(true, true).show(1000).fadeOut(7500);
     }
 }
+document.addEventListener('keydown',function(e){if(e.key==='Enter'){calculate()}});
+$('.tab').on('click',()=>{$('.tab').blur()});
 function removeLastInput() {
     display.value = display.value.slice(0, -1)
 }
@@ -61,12 +63,12 @@ function showContas()
     for(let i = 0; i < contas.length; i++)
     {
         list.innerHTML += `<tr scope="row">
-            <td id='resultado' onclick="addDisplay('${contas[i]['conta']}')" tabindex="0">
+            <td id='resultado' onclick="addDisplay('${contas[i]['conta']}')" tabindex="-1">
                 <span id='calculum' tabindex="0">${contas[i]['conta']}</span>
             </td>
-            <td id='botao_trash' tabindex="0">
-                <button id='btn-trash' onclick='removeConta("${contas[i]['conta']}")'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <td id='botao_trash' tabindex="-1">
+                <button tabindex="0" id='btn-trash' onclick='removeConta("${contas[i]['conta']}")'>
+                    <svg tabindex="0" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                     </svg>
